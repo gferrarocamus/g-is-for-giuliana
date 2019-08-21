9937a9f41a40c8d8b10b027f1fa1f00ab9d9cb6d
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-08-21 13:56:09 -0300

    Add styles to card

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React from 'react';
 import './App.css';
 import linkedin from './img/linkedin.svg';
 import github from './img/github.svg';
-// import add from './img/user-plus.svg';
 import connect from './img/message-square.svg';
 
 function App() {
   return (
     <div className="App">
       <div className="card-border">
-        <div className="card-contents">
-          <h1>Giuliana Ferraro</h1>
-          <h2>Full-Stack Web Developer</h2>
-          <div className="icons">
-            <a href=""><img src={linkedin} alt="LinkedIn"></img></a>
-            | <a href=""><img src={github} alt="GitHub"></img></a>
-            | <a href=""><img src={connect} alt="Skype"></img></a>
+        <div className="card-container">
+          <div className="card-contents">
+            <h1>Giuliana Ferraro</h1>
+            <h2>Full-Stack Web Developer</h2>
+            <div className="icons">
+              <a href="https://www.linkedin.com/in/giulianaferraro/en"><img src={linkedin} alt="LinkedIn" /></a>
+              {'| '}
+              <a href="https://github.com/gferrarocamus"><img src={github} alt="GitHub" /></a>
+              {'| '}
+              <a href="https://join.skype.com/invite/wyL8Xviy3Af0"><img src={connect} alt="Skype" /></a>
+            </div>
           </div>
         </div>
       </div>
