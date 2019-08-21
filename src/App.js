0d5dc0687d7f9e5531da3add648941861dc345dc
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-08-21 11:56:17 -0300

    Add markup and some styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import linkedin from './img/linkedin.svg';
+import github from './img/github.svg';
+// import add from './img/user-plus.svg';
+import connect from './img/message-square.svg';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="card-border">
+        <div className="card-contents">
+          <h1>Giuliana Ferraro</h1>
+          <h2>Full-Stack Web Developer</h2>
+          <div className="icons">
+            <a href=""><img src={linkedin} alt="LinkedIn"></img></a>
+            | <a href=""><img src={github} alt="GitHub"></img></a>
+            | <a href=""><img src={connect} alt="Skype"></img></a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
