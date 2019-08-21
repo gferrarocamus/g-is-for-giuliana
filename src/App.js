ead680df9335ee499bab6c6504c8c64e08f6c9ae
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-08-21 17:13:57 -0300

    Finish animations

--- src/App.js
+++ src/App.js
@@ -7,18 +7,25 @@ import connect from './img/message-square.svg';
 function App() {
   return (
     <div className="App">
-      {/* <img src="https://image.flaticon.com/icons/svg/1168/1168004.svg" /> */}
-      <div className="card-border">
-        <div className="card-container">
-          <div className="card-contents">
-            <h1>Giuliana Ferraro</h1>
-            <h2>Full-Stack Web Developer</h2>
-            <div className="icons">
-              <a href="https://www.linkedin.com/in/giulianaferraro/en"><img src={linkedin} alt="LinkedIn" /></a>
-              {'| '}
-              <a href="https://github.com/gferrarocamus"><img src={github} alt="GitHub" /></a>
-              {'| '}
-              <a href="https://join.skype.com/invite/wyL8Xviy3Af0"><img src={connect} alt="Skype" /></a>
+      <div className="card">
+        <div className="card-border card__side card__side--front">
+          <div className="card-container">
+            <div className="card-contents">
+              <h1>Giuliana Ferraro</h1>
+              <h2>Full-Stack Web Developer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="card-border card__side card__side--back">
+          <div className="card-container">
+            <div className="card-contents">
+              <div className="icons">
+                <a href="https://www.linkedin.com/in/giulianaferraro/en"><img src={linkedin} alt="LinkedIn" /></a>
+                {'| '}
+                <a href="https://github.com/gferrarocamus"><img src={github} alt="GitHub" /></a>
+                {'| '}
+                <a href="https://join.skype.com/invite/wyL8Xviy3Af0"><img src={connect} alt="Skype" /></a>
+              </div>
             </div>
           </div>
         </div>
