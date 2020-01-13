888dee86acfc2d33dc43b936f1c7800be9e4d5c9
giulianaferraro <giulianaferrarocamus@gmail.com>
2020-01-13 13:35:40 +0100

    Updates

---
+++ precache-manifest.ebd18a8381b2bfe4cb8e65b126c46e83.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b60d0bfaa33a3464f5c387f9f49a50",
+    "url": "/g-is-for-giuliana/index.html"
+  },
+  {
+    "revision": "583098de41b4701c8758",
+    "url": "/g-is-for-giuliana/static/css/main.16c272f9.chunk.css"
+  },
+  {
+    "revision": "a6a4ed08df6121d91bba",
+    "url": "/g-is-for-giuliana/static/js/2.6da9e995.chunk.js"
+  },
+  {
+    "revision": "583098de41b4701c8758",
+    "url": "/g-is-for-giuliana/static/js/main.4a130129.chunk.js"
+  },
+  {
+    "revision": "ef311551a172ee5c4273",
+    "url": "/g-is-for-giuliana/static/js/runtime~main.aab93747.js"
+  },
+  {
+    "revision": "5cff78e44ca42e5026f832f7b41fb018",
+    "url": "/g-is-for-giuliana/static/media/github.5cff78e4.svg"
+  },
+  {
+    "revision": "beb7135630c8d3725bc3571be62ad56a",
+    "url": "/g-is-for-giuliana/static/media/linkedin.beb71356.svg"
+  },
+  {
+    "revision": "42f61b6cfe834007f261ed563b6d3ad3",
+    "url": "/g-is-for-giuliana/static/media/message-square.42f61b6c.svg"
+  },
+  {
+    "revision": "4af98ea7f8ec35d4b2f2ac7e22f2ccb4",
+    "url": "/g-is-for-giuliana/static/media/mouse-pointer.4af98ea7.svg"
+  }
+]);
